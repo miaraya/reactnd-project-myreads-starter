e455cf36acd59b7acfe8cf8ee25c9efd85858bbd
Miguel Araya <miaraya@gmail.com>
2020-07-17 01:15:21 +0700

    search book, add to shelves

--- src/App.js
+++ src/App.js
@@ -20,6 +20,19 @@ class BooksApp extends React.Component {
       books: currentState.books,
     }));
   };
+  addBook = (book) => {
+    const aux = this.state.books.find((x) => x.id === book.id);
+    if (aux) {
+      aux.shelf = book.shelf;
+      this.setState((currentState) => ({
+        books: currentState.books,
+      }));
+    } else {
+      this.setState((currentState) => ({
+        books: currentState.books.concat([book]),
+      }));
+    }
+  };
 
   render() {
     const { books } = this.state;
@@ -43,6 +56,7 @@ class BooksApp extends React.Component {
               goBack={() => {
                 history.push("/");
               }}
+              handleChange={this.addBook}
             />
           )}
         />
@@ -51,20 +65,61 @@ class BooksApp extends React.Component {
   }
 }
 
-const BookSearch = (props) => {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <button className="close-search" onClick={() => props.goBack()}>
-          Close
-        </button>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" />
+class BookSearch extends Component {
+  state = { books: [], query: "" };
+  doSearch = (query) => {
+    this.setState({ query });
+    BooksAPI.search(query).then((books) => {
+      books !== undefined &&
+        this.setState(() => ({
+          books: books.error ? [] : books,
+        }));
+
+      console.log(books);
+    });
+  };
+  handleChange = (book) => {
+    this.props.handleChange(book);
+  };
+
+  render() {
+    const { books } = this.state;
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <button className="close-search" onClick={() => this.props.goBack()}>
+            Close
+          </button>
+          <SearchInput doSearch={this.doSearch} />
+        </div>
+
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {books.map((book) => {
+              return (
+                <li key={book.id}>
+                  <Book book={book} handleChange={this.handleChange} />
+                </li>
+              );
+            })}
+          </ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid" />
-      </div>
+    );
+  }
+}
+
+const SearchInput = (props) => {
+  const handleChange = (event) => {
+    props.doSearch(event.target.value);
+  };
+  return (
+    <div className="search-books-input-wrapper">
+      <input
+        type="text"
+        placeholder="Search by title or author"
+        onChange={handleChange}
+      />
     </div>
   );
 };
@@ -72,7 +127,7 @@ const BookSearch = (props) => {
 const BookList = (props) => {
   const { books } = props;
 
-  const handleChange = () => {
+  const handleChange = (book) => {
     props.shelfChange();
   };
   return (
@@ -106,32 +161,30 @@ const BookList = (props) => {
   );
 };
 
-class BookShelf extends Component {
-  handleChange = () => {
-    this.props.handleChange();
+const BookShelf = (props) => {
+  const handleChange = (book) => {
+    props.handleChange(book);
   };
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books &&
-              this.props.books.map((book) => (
-                <li key={book.id}>
-                  <Book book={book} handleChange={this.handleChange} />
-                </li>
-              ))}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books &&
+            props.books.map((book) => (
+              <li key={book.id}>
+                <Book book={book} handleChange={handleChange} />
+              </li>
+            ))}
+        </ol>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const Book = (props) => {
-  const handleChange = () => {
-    props.handleChange();
+  const handleChange = (book) => {
+    props.handleChange(book);
   };
   return (
     <div className="book">
@@ -141,36 +194,51 @@ const Book = (props) => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage: `url("${props.book.imageLinks.smallThumbnail}")`,
+            backgroundImage:
+              props.book.imageLinks &&
+              `url("${props.book.imageLinks.smallThumbnail}")`,
           }}
         />
         <BookMenu book={props.book} handleChange={handleChange} />
       </div>
       <div className="book-title">{props.book.title}</div>
-      {props.book.authors.map((a) => (
-        <div key={a} className="book-authors">
-          {a}
-        </div>
-      ))}
+      {props.book.authors ? (
+        props.book.authors.map((a) => (
+          <div key={a} className="book-authors">
+            {a}
+          </div>
+        ))
+      ) : (
+        <div className="book-authors">{"Unknown Author"}</div>
+      )}
     </div>
   );
 };
 
 const BookMenu = (props) => {
+  const shelves = [
+    { value: "move", text: "Move to...", disabled: true },
+    { value: "currentlyReading", text: "Currently Reading", disabled: false },
+    { value: "wantToRead", text: "Want to Read", disabled: false },
+    { value: "read", text: "Read", disabled: false },
+    { value: "none", text: "None", disabled: false },
+  ];
   const handleMenuChange = (event) => {
-    props.book.shelf = event.target.value;
-    props.handleChange();
+    props.book.shelf =
+      event.target.value !== "none" ? event.target.value : props.book.shelf;
+    props.handleChange(props.book);
   };
   return (
     <div className="book-shelf-changer">
-      <select onChange={handleMenuChange} defaultValue="none">
-        <option value="move" disabled>
-          Move to...
-        </option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
+      <select
+        onChange={handleMenuChange}
+        defaultValue={props.book.shelf ? props.book.shelf : "none"}
+      >
+        {shelves.map((o) => (
+          <option key={o.value} value={o.value} disabled={o.disabled}>
+            {o.text}
+          </option>
+        ))}
       </select>
     </div>
   );
